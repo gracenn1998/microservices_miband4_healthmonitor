e274845d418427ccc5c87eef89c0323f43009bc0
gracenn1998 <grace.nn1998@gmail.com>
2020-11-25 22:26:30 +0900

    added cors

--- user_database/__init__.py
+++ user_database/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 POSTGRES = {
